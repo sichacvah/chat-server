42bad00b78a6db6abdeb5c740eba2a6191cd18ed
sichacvah <sichacvah@localhost.localdomain>
2016-11-01 07:19:36 +0500

    init

---
+++ web/channels/user_socket.ex
@@ -0,0 +1,25 @@
+defmodule PhoenixChat.UserSocket do
+  use Phoenix.Socket
+
+  ## Channels
+  channel "rooms:*", PhoenixChat.RoomChannel
+
+  transport :websocket, Phoenix.Transports.WebSocket
+  transport :longpoll, Phoenix.Transports.LongPoll
+
+  def connect(_params, socket) do
+    {:ok, socket}
+  end
+
+  # Socket id's are topics that allow you to identify all sockets for a given user:
+  #
+  #     def id(socket), do: "users_socket:#{socket.assigns.user_id}"
+  #
+  # Would allow you to broadcast a "disconnect" event and terminate
+  # all active sockets and channels for a given user:
+  #
+  #     PhoenixChat.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
+  #
+  # Returning `nil` makes this socket anonymous.
+  def id(_socket), do: nil
+end
